9b5590489779efa06942ecdee89293ddbda30195
vaishanth_r <vaishanth.rmrj@gmail.com>
2025-05-10 23:50:47 +0000

    update docker files

--- docker/dev/ros_entrypoint.sh
+++ docker/dev/ros_entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/bash
 set -e
 
+# Set ROS to use port 11411
+export ROS_MASTER_URI=http://localhost:11411
+export ROS_HOSTNAME=localhost
+
 # setup ros environment
 source "/opt/ros/noetic/setup.bash"
 
